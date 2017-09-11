9d8c2adf81154ce74283c4cd86fd478c57aec608
romangurevitch <romang@jfrog.com>
2017-09-11 10:45:51 +0300

    Compatibility fix for IntelliJ IDEA 2017.3

--- plugin/src/main/java/org/jfrog/idea/ui/utils/IconUtils.java
+++ plugin/src/main/java/org/jfrog/idea/ui/utils/IconUtils.java
@@ -1,15 +1,8 @@
 package org.jfrog.idea.ui.utils;
 
 import com.intellij.openapi.util.IconLoader;
-import org.apache.batik.transcoder.TranscoderException;
-import org.apache.batik.transcoder.TranscoderInput;
-import org.apache.batik.transcoder.TranscoderOutput;
-import org.apache.batik.transcoder.image.ImageTranscoder;
-import org.apache.batik.transcoder.image.PNGTranscoder;
 
 import javax.swing.*;
-import java.awt.image.BufferedImage;
-import java.io.InputStream;
 
 /**
  * Created by romang on 4/12/17.
@@ -20,42 +13,9 @@ public class IconUtils {
 
     public static Icon load(String severity) {
         try {
-            InputStream stream = IconUtils.class.getResourceAsStream("/icons/" + severity.toLowerCase() + ".svg");
-            BufferedImage image = loadImage(stream, 10, 10);
-            return new ImageIcon(image);
+            return IconLoader.findIcon("/icons/" + severity.toLowerCase() + ".png");
         } catch (Exception e) {
             return defaultIcon;
         }
-    }
-
-    private static BufferedImage loadImage(InputStream svgFile, float width, float height) throws TranscoderException {
-        BufferedImageTranscoder imageTranscoder = new BufferedImageTranscoder();
-
-        imageTranscoder.addTranscodingHint(PNGTranscoder.KEY_WIDTH, width);
-        imageTranscoder.addTranscodingHint(PNGTranscoder.KEY_HEIGHT, height);
-
-        TranscoderInput input = new TranscoderInput(svgFile);
-        imageTranscoder.transcode(input, null);
-
-        return imageTranscoder.getBufferedImage();
-    }
-
-    private static class BufferedImageTranscoder extends ImageTranscoder {
-        @Override
-        public BufferedImage createImage(int w, int h) {
-            BufferedImage bi = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-            return bi;
-        }
-
-        @Override
-        public void writeImage(BufferedImage img, TranscoderOutput output) {
-            this.img = img;
-        }
-
-        public BufferedImage getBufferedImage() {
-            return img;
-        }
-
-        private BufferedImage img = null;
     }
 }
