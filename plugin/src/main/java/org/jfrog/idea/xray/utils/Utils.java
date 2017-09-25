a9ef5322d67008adfce350f6428eaa62bf788eb8
romangurevitch <romang@jfrog.com>
2017-09-25 15:29:29 +0300

    Added try with resource on checksum calculation

--- plugin/src/main/java/org/jfrog/idea/xray/utils/Utils.java
+++ plugin/src/main/java/org/jfrog/idea/xray/utils/Utils.java
@@ -49,21 +49,20 @@ public class Utils {
     @NotNull
     private static String calculateChecksum(File file, String algorithm) throws IOException, NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance(algorithm);
-        FileInputStream fis = new FileInputStream(file);
+        try (FileInputStream fis = new FileInputStream(file)) {
+            byte[] dataBytes = new byte[1024];
+            int nread;
+            while ((nread = fis.read(dataBytes)) != -1) {
+                md.update(dataBytes, 0, nread);
+            }
+            byte[] mdbytes = md.digest();
 
-        byte[] dataBytes = new byte[1024];
-
-        int nread = 0;
-        while ((nread = fis.read(dataBytes)) != -1) {
-            md.update(dataBytes, 0, nread);
+            // convert the byte to hex format method 1
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < mdbytes.length; i++) {
+                sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            return sb.toString();
         }
-        byte[] mdbytes = md.digest();
-
-        // convert the byte to hex format method 1
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < mdbytes.length; i++) {
-            sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
-        }
-        return sb.toString();
     }
 }
