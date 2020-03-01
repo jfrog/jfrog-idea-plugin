c26bd0c911da0c7e1c18a44218d1936bca60ec99
barbelity <barb@jfrog.com>
2020-03-01 16:01:59 +0200

    Auto refresh dependencies tree in npm and go projects

--- src/main/java/com/jfrog/ide/idea/scan/GoScanManager.java
+++ src/main/java/com/jfrog/ide/idea/scan/GoScanManager.java
@@ -5,9 +5,6 @@ import com.intellij.openapi.externalSystem.model.project.LibraryDependencyData;
 import com.intellij.openapi.externalSystem.model.project.ProjectData;
 import com.intellij.openapi.externalSystem.service.project.ExternalProjectRefreshCallback;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFileManager;
-import com.intellij.openapi.vfs.newvfs.BulkFileListener;
-import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
 import com.intellij.util.EnvironmentUtil;
 import com.jfrog.ide.common.go.GoTreeBuilder;
 import com.jfrog.ide.common.scan.ComponentPrefix;
@@ -16,15 +13,10 @@ import com.jfrog.ide.idea.ui.filters.FilterManagerService;
 import com.jfrog.ide.idea.ui.issues.IssuesTree;
 import com.jfrog.ide.idea.ui.licenses.LicensesTree;
 import com.jfrog.ide.idea.utils.Utils;
-import org.apache.commons.lang.StringUtils;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Collection;
-import java.util.List;
 
 /**
  * Created by Bar Belity on 06/02/2020.
@@ -32,7 +24,6 @@ import java.util.List;
 public class GoScanManager extends ScanManager {
 
     private GoTreeBuilder goTreeBuilder;
-    private String sumFile;
 
     /**
      * @param mainProject - Currently opened IntelliJ project. We'll use this project to retrieve project based services
@@ -43,7 +34,7 @@ public class GoScanManager extends ScanManager {
         super(mainProject, project, ComponentPrefix.GO);
         getLog().info("Found go project: " + getProjectName());
         goTreeBuilder = new GoTreeBuilder(Utils.getProjectBasePath(project), EnvironmentUtil.getEnvironmentMap(), getLog());
-        subscribeGoSumChangeEvents(mainProject);
+        subscribeLaunchDependencyScanOnFileChangedEvents("go.sum");
     }
 
     @Override
@@ -54,23 +45,5 @@ public class GoScanManager extends ScanManager {
     @Override
     protected void buildTree(@Nullable DataNode<ProjectData> externalProject) throws IOException {
         setScanResults(goTreeBuilder.buildTree());
-    }
-
-    private void subscribeGoSumChangeEvents(Project mainProject) {
-        Path goSumPath = Paths.get(project.getBasePath(), "go.sum");
-        this.sumFile = goSumPath.toString();
-
-        // Register for file change event of go.sum file.
-        mainProject.getMessageBus().connect().subscribe(VirtualFileManager.VFS_CHANGES, new BulkFileListener() {
-            @Override
-            public void after(@NotNull List<? extends VFileEvent> events) {
-                for (VFileEvent event : events) {
-                    String filePath = event.getPath();
-                    if (StringUtils.equals(filePath, sumFile)) {
-                        asyncScanAndUpdateResults();
-                    }
-                }
-            }
-        });
     }
 }
