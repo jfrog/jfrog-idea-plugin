8190944dda7f6cf67d3c48dd99b158b371c91c32
yahavi <yahavi@jfrog.com>
2021-10-03 11:59:17 +0300

    Set the JAVA_HOME from Gradle settings

--- src/main/java/com/jfrog/ide/idea/scan/GradleScanManager.java
+++ src/main/java/com/jfrog/ide/idea/scan/GradleScanManager.java
@@ -1,5 +1,6 @@
 package com.jfrog.ide.idea.scan;
 
+import com.google.common.collect.Maps;
 import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
@@ -15,6 +16,7 @@ import com.jfrog.ide.idea.inspections.GradleKotlinInspection;
 import com.jfrog.ide.idea.log.Logger;
 import com.jfrog.ide.idea.ui.ComponentsTree;
 import com.jfrog.ide.idea.ui.filters.filtermanager.ConsistentFilterManager;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.SystemUtils;
 import org.jetbrains.plugins.gradle.service.GradleInstallationManager;
 import org.jetbrains.plugins.gradle.service.settings.GradleConfigurable;
@@ -26,6 +28,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
 /**
  * Created by Yahav Itzhak on 9 Nov 2017.
@@ -43,7 +46,8 @@ public class GradleScanManager extends ScanManager {
     GradleScanManager(Project project, String basePath) throws IOException {
         super(project, basePath, ComponentPrefix.GAV);
         getLog().info("Found Gradle project: " + getProjectName());
-        gradleTreeBuilder = new GradleTreeBuilder(Paths.get(basePath), EnvironmentUtil.getEnvironmentMap(), getGradleExe());
+        Map<String, String> env = Maps.newHashMap(EnvironmentUtil.getEnvironmentMap());
+        gradleTreeBuilder = new GradleTreeBuilder(Paths.get(basePath), env, getGradleExeAndJdk(env));
     }
 
     @Override
@@ -75,10 +79,11 @@ public class GradleScanManager extends ScanManager {
     /**
      * Extracts the chosen Gradle executable path from the Gradle plugin. If Gradle is not installed, throw an exception.
      *
+     * @param env - The environment variables to set the JAVA_HOME
      * @return the chosen Gradle executable path
      */
-    private String getGradleExe() {
-        File gradleHome = resolveGradleHome();
+    private String getGradleExeAndJdk(Map<String, String> env) {
+        File gradleHome = resolveGradleAndSetJavaHome(env);
         if (gradleHome == null) {
             getLog().info("Using Gradle from system path.");
             return null;
@@ -88,7 +93,13 @@ public class GradleScanManager extends ScanManager {
         return gradleExe;
     }
 
-    private File resolveGradleHome() {
+    /**
+     * Resolve Gradle executable and Java home from Gradle settings.
+     *
+     * @param env - The environment variables to set the JAVA_HOME
+     * @return gradle executable
+     */
+    private File resolveGradleAndSetJavaHome(Map<String, String> env) {
         GradleProjectSettings projectSettings = GradleSettings.getInstance(project).getLinkedProjectsSettings().stream().findAny().orElse(null);
         if (projectSettings == null) {
             getLog().error("Couldn't retrieve Gradle project settings. Hint - make sure the Gradle project was properly imported.");
@@ -97,8 +108,15 @@ public class GradleScanManager extends ScanManager {
         GradleInstallationManager gradleInstallationManager = ServiceManager.getService(GradleInstallationManager.class);
         File gradleHome = gradleInstallationManager.getGradleHome(project, projectSettings.getExternalProjectPath());
         if (gradleHome != null) {
+            // Gradle seems to set properly, try to set JAVA_HOME environment variable.
+            String javaHome = gradleInstallationManager.getGradleJvmPath(project, projectSettings.getExternalProjectPath());
+            if (StringUtils.isNotBlank(javaHome)) {
+                getLog().info("Using Java home: " + javaHome);
+                env.put("JAVA_HOME", javaHome);
+            }
             return gradleHome;
         }
+        // Gradle wasn't set properly
         if (isMisconfigurationError(projectSettings.getExternalProjectPath())) {
             Logger.openSettings("It looks like Gradle home was not properly set in your project. " +
                     "Click <a href=\"#settings\">here</a> to set Gradle home.", project, GradleConfigurable.class);
