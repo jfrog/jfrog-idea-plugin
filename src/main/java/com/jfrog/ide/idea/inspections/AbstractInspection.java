af99a2b037fc218059d89650ac3274046845e369
yahavi <yahavi@jfrog.com>
2021-12-27 16:33:22 +0200

    Accept CR suggestions

--- src/main/java/com/jfrog/ide/idea/inspections/AbstractInspection.java
+++ src/main/java/com/jfrog/ide/idea/inspections/AbstractInspection.java
@@ -45,8 +45,8 @@ public abstract class AbstractInspection extends LocalInspectionTool implements 
      *
      * @param problemsHolder - The "Show in dependency tree" option will be registered in this container.
      * @param element        - The Psi element in the package descriptor
-     * @param isOnTheFly     - True if the inspection triggered by opening a package descriptor file.
-     *                       False if the inspection triggered manually by clicking on "Code | Inspect Code".
+     * @param isOnTheFly     - True if the inspection was triggered by opening a package descriptor file.
+     *                       False if the inspection was triggered manually by clicking on "Code | Inspect Code".
      */
     void visitElement(ProblemsHolder problemsHolder, PsiElement element, boolean isOnTheFly) {
         if (!isOnTheFly) {
