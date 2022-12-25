9e40fd322c0ca6fc050a488282a01490f2b0047a
Or Geva <orgab@jfrog.com>
2022-12-25 12:03:56 +0200

    Skip null values for webview message passing

--- src/main/java/com/jfrog/ide/idea/ui/jcef/message/MessagePipeSupport.java
+++ src/main/java/com/jfrog/ide/idea/ui/jcef/message/MessagePipeSupport.java
@@ -5,10 +5,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
+import static com.jfrog.ide.common.utils.Utils.createMapper;
+
 public class MessagePipeSupport {
 
     public static String Pack(String type, Object data) throws JsonProcessingException {
-        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        ObjectMapper ow = createMapper();
         return ow.writeValueAsString(new PackedMessage(type, data));
     }
 }
