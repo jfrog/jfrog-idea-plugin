af18d43c5f39f1e2d1d40da08b1bebf387fc1de5
Asaf Gabai <asafg@jfrog.com>
2023-01-02 16:21:10 +0200

    Test fixes

--- src/test/java/com/jfrog/ide/idea/scan/GradleScanManagerTest.java
+++ src/test/java/com/jfrog/ide/idea/scan/GradleScanManagerTest.java
@@ -3,13 +3,9 @@ package com.jfrog.ide.idea.scan;
 import com.intellij.testFramework.HeavyPlatformTestCase;
 import com.intellij.util.ConcurrencyUtil;
 import com.jfrog.ide.common.gradle.GradleDriver;
-import com.jfrog.ide.common.scan.GraphScanLogic;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.plugins.gradle.service.project.open.GradleProjectImportUtil;
-import org.jfrog.build.api.util.NullLog;
-import org.jfrog.build.extractor.scan.DependencyTree;
-import org.jfrog.build.extractor.scan.Scope;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,9 +14,6 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
-
-import static com.jfrog.ide.idea.TestUtils.assertScopes;
-import static com.jfrog.ide.idea.TestUtils.getAndAssertChild;
 
 /**
  * @author yahavi
@@ -69,30 +62,5 @@ public class GradleScanManagerTest extends HeavyPlatformTestCase {
         assertEquals(System.getenv("JAVA_HOME"), env.get("JAVA_HOME"));
         assertNotNull(gradleExe);
         new GradleDriver(gradleExe, null).verifyGradleInstalled();
-    }
-
-    public void testBuildTree() throws IOException {
-        GradleProjectImportUtil.linkAndRefreshGradleProject(globalProjectDir, getProject());
-        GradleScanManager gradleScanManager = new GradleScanManager(getProject(), globalProjectDir, executorService);
-        gradleScanManager.setScanLogic(new GraphScanLogic(null, new NullLog()));
-        gradleScanManager.buildTree();
-
-        // Run and check scan results
-        DependencyTree results = gradleScanManager.getScanResults();
-        assertNotNull(results);
-        assertTrue(results.isMetadata());
-        assertEquals(Paths.get(globalProjectDir).getFileName().toString(), results.getUserObject());
-        assertScopes(results);
-        assertEquals(3, results.getChildCount());
-
-        // Check module dependency
-        DependencyTree moduleNode = getAndAssertChild(results, "shared");
-        assertTrue(results.isMetadata());
-        assertEquals(1, moduleNode.getChildCount());
-
-        // Check dependency
-        DependencyTree dependencyNode = getAndAssertChild(moduleNode, "junit:junit:4.7");
-        assertFalse(dependencyNode.isMetadata());
-        assertContainsElements(dependencyNode.getScopes(), new Scope("TestImplementation"), new Scope("TestRuntimeClasspath"), new Scope("TestCompileClasspath"));
     }
 }
