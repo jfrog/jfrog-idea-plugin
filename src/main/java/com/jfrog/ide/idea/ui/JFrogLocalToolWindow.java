e26194fe6fa5065c1a33e7956f329573a4d19005
Yahav Itzhak <yahavi@users.noreply.github.com>
2023-01-09 18:28:57 +0200

    Add Gradle task to download the new webview (#247)

--- src/main/java/com/jfrog/ide/idea/ui/JFrogLocalToolWindow.java
+++ src/main/java/com/jfrog/ide/idea/ui/JFrogLocalToolWindow.java
@@ -29,7 +29,7 @@ import com.jfrog.ide.idea.log.Logger;
 import com.jfrog.ide.idea.ui.jcef.message.MessagePacker;
 import com.jfrog.ide.idea.ui.utils.ComponentUtils;
 import com.jfrog.ide.idea.ui.webview.WebviewObjectConverter;
-import org.apache.commons.io.FileUtils;
+import com.jfrog.ide.idea.utils.Utils;
 import org.cef.browser.CefBrowser;
 import org.cef.browser.CefFrame;
 import org.cef.handler.CefLoadHandlerAdapter;
@@ -40,6 +40,7 @@ import javax.swing.tree.TreeSelectionModel;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -118,9 +119,8 @@ public class JFrogLocalToolWindow extends AbstractJFrogToolWindow {
         try {
             tempDirPath = Files.createTempDirectory("jfrog-idea-plugin");
             tempDirPath.toFile().deleteOnExit();
-            FileUtils.copyToFile(getClass().getResourceAsStream("/jfrog-ide-webview-container/index.html"), new File(tempDirPath.toString(), "index.html"));
-            FileUtils.copyToFile(getClass().getResourceAsStream("/jfrog-ide-webview-container/bundle.js"), new File(tempDirPath.toString(), "bundle.js"));
-        } catch (IOException e) {
+            Utils.extractFromResources("/jfrog-ide-webview", tempDirPath);
+        } catch (IOException | URISyntaxException e) {
             Logger.getInstance().error(e.getMessage());
             return;
         }
