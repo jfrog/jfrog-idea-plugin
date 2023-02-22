146145790e612cb0bafd1692844d9f9bcf925d5a
Michael Sverdlov <michaelsv@jfrog.com>
2023-02-22 14:31:38 +0200

    comom

--- src/test/java/com/jfrog/ide/idea/inspections/InspectionToolsTest.java
+++ src/test/java/com/jfrog/ide/idea/inspections/InspectionToolsTest.java
@@ -1,31 +1,20 @@
 package com.jfrog.ide.idea.inspections;
 
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertEquals;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
 
 /**
  * @author michaels
  */
 
-public class InspectionToolsTest {
+public class InspectionToolsTest extends LightJavaCodeInsightFixtureTestCase {
 
-    @DataProvider
-    private Object[][] goVersionProvider() {
-        return new Object[][]{
-                {"1.0", "1.0"},
-                {"", "(,1.0]"},
-                {"", "(,1.0)"},
-                {"1.0", "[1.0]"},
-                {"", "(1.0,)"},
-                {"", "(1.0, 2.0)"},
-                {"1.0", "[1.0, 2.0]"},
-        };
-    }
-
-    @Test(dataProvider = "goVersionProvider")
-    public void testConvertFixVersionStringToMinFixVersion(String versionOutput, String expectedVersion) {
-        assertEquals(expectedVersion, AbstractInspection.convertFixVersionStringToMinFixVersion(versionOutput));
+    public void testConvertFixVersionStringToMinFixVersion() {
+        assertEquals("1.0", AbstractInspection.convertFixVersionStringToMinFixVersion("1.0"));
+        assertEquals("", AbstractInspection.convertFixVersionStringToMinFixVersion("(,1.0]"));
+        assertEquals("", AbstractInspection.convertFixVersionStringToMinFixVersion("(,1.0)"));
+        assertEquals("1.0", AbstractInspection.convertFixVersionStringToMinFixVersion("[1.0]"));
+        assertEquals("", AbstractInspection.convertFixVersionStringToMinFixVersion("(1.0,)"));
+        assertEquals("", AbstractInspection.convertFixVersionStringToMinFixVersion("(1.0, 2.0)"));
+        assertEquals("1.0", AbstractInspection.convertFixVersionStringToMinFixVersion("[1.0, 2.0]"));
     }
 }
