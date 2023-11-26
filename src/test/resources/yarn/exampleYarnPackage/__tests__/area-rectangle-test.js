fe6ad01516578da508b38e4fcbb4ecaaa1c30eff
noys <noys@jfrog.com>
2023-11-26 11:54:00 +0200

    :truck: removed unnecessary files for tests.

--- src/test/resources/yarn/exampleYarnPackage/__tests__/area-rectangle-test.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-test('area of rectangle with width 3 and height 4 to equal 12', () => {
-    const ShapeMath = require('../index.js');
-    expect(ShapeMath.area_rectangle(3, 4)).toBe(12);
-})
