fe559f4d212a5385db4b0c49374f208254cd0fb7
Or Toren <ort@jfrog.com>
2023-11-27 11:09:35 +0200

    SCANNER_BINARY_VERSION update

--- src/main/java/com/jfrog/ide/idea/scan/ScanBinaryExecutor.java
+++ src/main/java/com/jfrog/ide/idea/scan/ScanBinaryExecutor.java
@@ -72,7 +72,7 @@ public abstract class ScanBinaryExecutor {
     private static final int USER_NOT_ENTITLED = 31;
     private static final int NOT_SUPPORTED = 13;
     private static final String SCANNER_BINARY_NAME = "analyzerManager";
-    private static final String SCANNER_BINARY_VERSION = "1.3.2.2019257";
+    private static final String SCANNER_BINARY_VERSION = "1.4.0.2021123";
     private static final String BINARY_DOWNLOAD_URL = "xsc-gen-exe-analyzer-manager-local/v1/" + SCANNER_BINARY_VERSION;
     private static final String DOWNLOAD_SCANNER_NAME = "analyzerManager.zip";
     private static final String MINIMAL_XRAY_VERSION_SUPPORTED_FOR_ENTITLEMENT = "3.66.0";
