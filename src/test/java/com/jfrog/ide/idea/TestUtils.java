4afd409545ad037545fea0335ef757a7d3d157ed
attiasas <assafattias93@gmail.com>
2026-05-27 16:57:56 +0300

    remove more unused

--- src/test/java/com/jfrog/ide/idea/TestUtils.java
+++ src/test/java/com/jfrog/ide/idea/TestUtils.java
@@ -2,13 +2,9 @@ package com.jfrog.ide.idea;
 
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.util.PsiTreeUtil;
 import com.jfrog.ide.common.deptree.DepTree;
 import com.jfrog.ide.common.deptree.DepTreeNode;
 import org.junit.Assert;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Bar Belity on 11/06/2020.
